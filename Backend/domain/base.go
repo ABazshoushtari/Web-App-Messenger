fdc7589df0709b6917e714d49bdc16feadeaa0aa
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2024-02-01 20:16:38 +0330

    feat: added custom context

--- Backend/domain/base.go
+++ Backend/domain/base.go
@@ -1,9 +1,24 @@
 package domain
 
-import "time"
+import (
+	"context"
+	"github.com/ABazshoushtari/Web-App-Messenger/domain/payloads"
+	"github.com/ABazshoushtari/Web-App-Messenger/internal/server/middleware"
+	"time"
+)
 
 type BaseModel struct {
 	ID        uint64    `json:"id,omitempty" gorm:"primarykey"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
 	UpdatedAt time.Time `json:"updated_at,omitempty"`
+}
+
+func GetUserDTO(ctx context.Context) *payloads.UserDTO {
+	if ctx == nil {
+		return nil
+	}
+	if c, ok := ctx.(middleware.CustomContext); ok {
+		return c.GetUser()
+	}
+	return nil
 }
--- Backend/internal/server/middleware/middlewares.go
+++ Backend/internal/server/middleware/middlewares.go
@@ -1,10 +1,14 @@
 package middleware
 
 import (
+	"context"
+	"github.com/ABazshoushtari/Web-App-Messenger/domain/payloads"
 	"github.com/ABazshoushtari/Web-App-Messenger/internal/logger"
 	"github.com/ABazshoushtari/Web-App-Messenger/repository"
 	"github.com/ABazshoushtari/Web-App-Messenger/service"
+	"github.com/labstack/echo/v4"
 	"go.uber.org/zap"
+	"net/http"
 )
 
 func NewMiddlewares(svcs *service.Services, repos *repository.Repositories) *Middlewares {
@@ -19,4 +23,28 @@ type Middlewares struct {
 	svcs  *service.Services
 	repos *repository.Repositories
 	log   *zap.SugaredLogger
+}
+
+type CustomContext struct {
+	context.Context
+	request func() *http.Request
+	user    *payloads.UserDTO
+}
+
+func (c *CustomContext) GetUser() *payloads.UserDTO {
+	return c.user
+}
+
+func (m *Middlewares) CustomContext() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			ctx := c.Request().Context()
+			ctx = &CustomContext{
+				Context: ctx,
+				request: c.Request,
+			}
+			c.SetRequest(c.Request().WithContext(ctx))
+			return next(c)
+		}
+	}
 }
