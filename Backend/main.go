22b3a6871c024d971de1cf3b77b50a4671e3949a
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2024-02-02 03:05:55 +0330

    fix: refactored main.go

--- Backend/main.go
+++ Backend/main.go
@@ -1,63 +1,16 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"net/http"
+	"github.com/ABazshoushtari/Web-App-Messenger/internal/config"
+	"github.com/ABazshoushtari/Web-App-Messenger/internal/logger"
+	"github.com/ABazshoushtari/Web-App-Messenger/internal/server"
+	"log"
 )
 
 func main() {
-	e := echo.New()
-
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	// CORS settings for your frontend, adjust as needed
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:3000"}, // Assuming your React app runs on this port
-		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
-	}))
-
-	// Routes
-	e.POST("/api/signup", signupHandler)
-
-	// Start server
-	e.Logger.Fatal(e.Start(":8080"))
-}
-
-func signupHandler(c echo.Context) error {
-	// Parse form data
-	//firstname := c.FormValue("firstname")
-	//lastname := c.FormValue("lastname")
-	//phone := c.FormValue("phone")
-	//username := c.FormValue("username")
-	//password := c.FormValue("password") // Remember to hash the password before storing it!
-	//bio := c.FormValue("bio")
-
-	// Parse file
-	file, err := c.FormFile("image")
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, "Failed to parse image")
+	if err := config.Load("config.yaml"); err != nil {
+		log.Fatal("error loading config")
 	}
-
-	// Open the file
-	src, err := file.Open()
-	if err != nil {
-		return err
-	}
-	defer src.Close()
-
-	// Here, you would usually save the file to your server or a cloud storage
-
-	// Validate and check for uniqueness of username and phone
-
-	// Create user logic here
-
-	// Generate JWT token logic here
-
-	return c.JSON(http.StatusCreated, map[string]interface{}{
-		"message": "User created successfully",
-		"token":   "your_jwt_token_here", // Replace with actual JWT token
-	})
+	logger.Init()
+	logger.Logger().Fatal(server.Start())
 }
