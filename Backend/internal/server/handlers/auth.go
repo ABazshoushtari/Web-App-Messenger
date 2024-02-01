7f8480e80249ff79657d40cd6284da610cc95013
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2024-02-01 20:30:41 +0330

    feat: added auth handler functions

---
+++ Backend/internal/server/handlers/auth.go
@@ -0,0 +1,71 @@
+package handlers
+
+import (
+	"errors"
+	"github.com/ABazshoushtari/Web-App-Messenger/domain/payloads"
+	"github.com/labstack/echo/v4"
+)
+
+func (h *Handlers) AuthRegister() echo.HandlerFunc {
+	type request struct {
+		payloads.UserRegisterRequest
+	}
+	type response struct {
+		payloads.UserRegisterResponse
+	}
+	return func(c echo.Context) error {
+		var req request
+		if err := c.Bind(&req); err != nil {
+			return errors.New("invalid form request")
+		}
+		//img, err := c.FormFile("image")
+		//if err != nil {
+		//	return errors.New("invalid user image")
+		//}
+		//username := c.FormValue("username")
+		//password := c.FormValue("password")
+		//firstName := c.FormValue("first_name")
+		//lastName := c.FormValue("last_name")
+		//phone := c.FormValue("phone")
+		//bio := c.FormValue("bio")
+		//
+		//req.Username = username
+		//req.Password = password
+		//req.FirstName = firstName
+		//req.LastName = lastName
+		//req.Phone = phone
+		//req.Image = img
+		//req.Bio = bio
+
+		res, err := h.svcs.User.AuthRegister(c.Request().Context(), req.UserRegisterRequest)
+		if err != nil {
+			return err
+		}
+		return c.JSON(200, response{
+			UserRegisterResponse: *res,
+		})
+	}
+}
+
+func (h *Handlers) AuthLogin() echo.HandlerFunc {
+	type request struct {
+		payloads.UserLoginRequest
+	}
+	type response struct {
+		payloads.UserLoginResponse
+	}
+	return func(c echo.Context) error {
+		var req request
+		if err := c.Bind(&req); err != nil {
+			return errors.New("invalid request body")
+		}
+
+		res, err := h.svcs.User.AuthLogin(c.Request().Context(), req.UserLoginRequest)
+		if err != nil {
+			return err
+		}
+		return c.JSON(200, response{
+			UserLoginResponse: *res,
+		})
+	}
+}
